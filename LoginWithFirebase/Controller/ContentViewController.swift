4b655f6afa3c471c0b590ac94bc75c56da0c0824
chenjiahau <ivan.chen@dlinkcorp.com>
2020-10-21 08:51:13 +0800

    Sign in, sign out and sign out.

---
+++ LoginWithFirebase/Controller/ContentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContentViewController.swift
+//  LoginWithFirebase
+//
+//  Created by chen Ivan on 2020/10/21.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
