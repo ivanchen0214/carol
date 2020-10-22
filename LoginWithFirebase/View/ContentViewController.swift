7c3e66326793016c3ad2af03ee716b115b4d8d97
chenjiahau <ivan.chen@dlinkcorp.com>
2020-10-22 08:35:49 +0800

    UITableView sample.

--- LoginWithFirebase/View/ContentViewController.swift
+++ LoginWithFirebase/View/ContentViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 import Firebase
 
 class ContentViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+  var dummyData: [String] = ["One", "Two", "Three"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
     title = Constants.title.content
     navigationItem.hidesBackButton = true
+    tableView.dataSource = self
   }
 
   @IBAction func pressLogout(_ sender: UIBarButtonItem) {
@@ -25,5 +28,17 @@ class ContentViewController: UIViewController {
     } catch let signOutError as NSError {
       print ("Error signing out: %@", signOutError)
     }
+  }
+}
+
+extension ContentViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return dummyData.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "dummyCellIdentifier", for: indexPath)
+    cell.textLabel?.text = dummyData[indexPath.row]
+    return cell
   }
 }
