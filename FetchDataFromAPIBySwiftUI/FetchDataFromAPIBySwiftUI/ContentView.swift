f9ef397b0bd84e5ff7e2e26e9682e229e1cb58b1
chenjiahau <ivan.chen@dlinkcorp.com>
2020-11-07 09:00:08 +0800

    Set UI.

--- FetchDataFromAPIBySwiftUI/FetchDataFromAPIBySwiftUI/ContentView.swift
+++ FetchDataFromAPIBySwiftUI/FetchDataFromAPIBySwiftUI/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State var cityName: String = ""
+  @State var keyword: String = ""
+  
+  var body: some View {
+    ZStack {
+      VStack {
+        VStack {
+          Text(self.cityName)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        }
+        Spacer()
+        VStack {
+          TextField("Enter your city name", text: $keyword)
+            .keyboardType(.default)
+          Divider()
+          Button(action: {
+            self.endEditing(true)
+            if keyword != "" {
+              self.cityName = keyword
+              keyword = ""
+            }
+          }) {
+            Text("Search")
+              .frame(minWidth: 0, maxWidth: .infinity)
+          }
+        }      }.padding()
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+extension View {
+  func endEditing(_ force: Bool) {
+    UIApplication.shared.windows.forEach { $0.endEditing(force)}
+  }
 }
