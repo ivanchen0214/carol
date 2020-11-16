d10a0ae16426705a3a9932117b9e627319ee6af5
chenjiahau <ivan.chen@dlinkcorp.com>
2020-11-16 11:33:02 +0800

    Edit, Delete

--- UseUITableViewControllerByStoryboard/UseUITableViewControllerByStoryboard/Controller/ListController.swift
+++ UseUITableViewControllerByStoryboard/UseUITableViewControllerByStoryboard/Controller/ListController.swift
@@ -5,29 +5,36 @@
 //  Created by chen Ivan on 2020/11/14.
 //
 
+// Core Data Path: /Library/Developer/CoreSimulator/Devices/B68B6F20-A1B2-4625-AFF2-263A5BA1C87D/data/Containers/Data/Application/7BA5A189-E6AD-43E3-BD21-B241C9779AA1/Library/Application Support
+
 import UIKit
+import CoreData
 
 class ListController: UITableViewController {
-  var alert: UIAlertController = UIAlertController(title: "Add A New Item", message: "", preferredStyle: UIAlertController.Style.alert)
-  var itemAry = [Item]()
+  let dataFilePath = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask)
+  //  let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+  let request: NSFetchRequest<Items> = Items.fetchRequest()
+  var actionType: String = ""
+  var addAlert: UIAlertController?
+  var editAlert: UIAlertController?
+  var deleteAlert: UIAlertController?
+  var itemAry = [Items]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    // print(dataFilePath)
     UINavigationBar.appearance().barTintColor = UIColor.blue
     UINavigationBar.appearance().tintColor = UIColor.white
     UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
     
     navigationItem.title = "List"
     
-//    for number in 0...20 {
-//      print(number)
-//      let newItem = Item()
-//      newItem.title = "asdf"
-//      newItem.selected = false
-//
-//      self.itemAry.append(newItem)
-//    }
+    do {
+      itemAry = try context.fetch(request)
+    } catch {
+      print("Error fetch data from context \(error)")
+    }
   }
   
   @IBAction func pressBackBtn(_ sender: UIBarButtonItem) {
@@ -35,37 +42,51 @@ class ListController: UITableViewController {
   }
   
   @IBAction func pressAddBtn(_ sender: UIBarButtonItem) {
-    alert = UIAlertController(title: "Add A New Item", message: "", preferredStyle: UIAlertController.Style.alert)
-    
-    alert.addTextField { (alertTextField) in
+    actionType = "ADD"
+    addAlert = UIAlertController(title: "Add A New Item", message: "", preferredStyle: UIAlertController.Style.alert)
+    addAlert?.addTextField { (alertTextField) in
+      alertTextField.keyboardType = UIKeyboardType.default
       alertTextField.placeholder = "something..."
       alertTextField.addTarget(self, action: #selector(ListController.alertTextFieldDidChange(_:)), for: UIControl.Event.editingChanged)
     }
-
+    
     let addAction = UIAlertAction(title: "Add", style: UIAlertAction.Style.default) { (UIAlertAction) in
-      let textField = self.alert.textFields![0]
+      let textField = self.addAlert?.textFields![0]
       
-      if let text = textField.text {
-        let newItem = Item()
+      if let text = textField?.text {
+        let newItem = Items(context: self.context)
         newItem.title = text
         newItem.selected = false
         self.itemAry.append(newItem)
+        
+        do {
+          try self.context.save()
+        } catch {
+          print("Error saving context \(error)")
+        }
+        
         self.tableView.reloadData()
       }
     }
     addAction.isEnabled = false
     
     let cancelAction = UIAlertAction(title: "Cancel", style:  UIAlertAction.Style.cancel) { (UIAlertAction) in
-      self.alert.dismiss(animated: true, completion: nil)
+      self.addAlert?.dismiss(animated: true, completion: nil)
     }
     
-    alert.addAction(addAction)
-    alert.addAction(cancelAction)
-    present(alert, animated: true, completion: nil)
+    addAlert?.addAction(addAction)
+    addAlert?.addAction(cancelAction)
+    present(addAlert!, animated: true, completion: nil)
   }
   
   @objc func alertTextFieldDidChange(_ sender: UITextField) {
-    alert.actions[0].isEnabled = sender.text!.count > 0
+    if actionType == "ADD" {
+      addAlert?.actions[0].isEnabled = sender.text!.count > 0
+    }
+    
+    if actionType == "EDIT" {
+      editAlert?.actions[0].isEnabled = sender.text!.count > 0
+    }
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -75,7 +96,7 @@ class ListController: UITableViewController {
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
     cell.textLabel?.text = self.itemAry[indexPath.row].title
-
+    
     if !itemAry[indexPath.row].selected {
       cell.accessoryType = UITableViewCell.AccessoryType.none
     } else {
@@ -89,5 +110,111 @@ class ListController: UITableViewController {
     itemAry[indexPath.row].selected = !itemAry[indexPath.row].selected
     tableView.reloadData()
     tableView.deselectRow(at: indexPath, animated: true)
+  }
+  
+  //- Editing cell, one button
+  //  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+  //    deleteAlert = UIAlertController(title: "Delete this one?", message: "", preferredStyle: UIAlertController.Style.alert)
+  //    deleteAlert?.message = itemAry[indexPath.row].title
+  //
+  //    let deleteAction = UIAlertAction(title: "Delete", style: UIAlertAction.Style.default) { (UIAlertAction) in
+  //      self.context.delete(self.itemAry[indexPath.row])
+  //      self.itemAry.remove(at: indexPath.row)
+  //
+  //      do {
+  //        try self.context.save()
+  //      } catch {
+  //        print("Error saving context \(error)")
+  //      }
+  //
+  //      tableView.reloadData()
+  //    }
+  //
+  //    let cancelAction = UIAlertAction(title: "Cancel", style:  UIAlertAction.Style.cancel) { (UIAlertAction) in
+  //      self.deleteAlert?.dismiss(animated: true, completion: nil)
+  //    }
+  //
+  //    deleteAlert?.addAction(deleteAction)
+  //    deleteAlert?.addAction(cancelAction)
+  //    present(deleteAlert!, animated: true, completion: nil)
+  //  }
+  //
+  //  override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+  //    return "Delete"
+  //  }
+  
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+  }
+  
+  override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+    let editAction = UIContextualAction(style: UIContextualAction.Style.normal, title: "Edit") { (action, view, completionHandler) in
+      self.actionType = "EDIT"
+      self.editAlert = UIAlertController(title: "Edit", message: "", preferredStyle: UIAlertController.Style.alert)
+      
+      self.editAlert?.addTextField { (alertTextField) in
+        alertTextField.keyboardType = UIKeyboardType.default
+        alertTextField.placeholder = "something..."
+        alertTextField.text = self.itemAry[indexPath.row].title
+        alertTextField.addTarget(self, action: #selector(ListController.alertTextFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+      }
+      
+      let editAction = UIAlertAction(title: "Edit", style: UIAlertAction.Style.default) { (UIAlertAction) in
+        let textField = self.editAlert?.textFields![0]
+        
+        if let text = textField?.text {
+          self.itemAry[indexPath.row].setValue(text, forKey: "title")
+
+          do {
+            try self.context.save()
+          } catch {
+            print("Error saving context \(error)")
+          }
+          
+          tableView.reloadData()
+        }
+      }
+      
+      let cancelAction = UIAlertAction(title: "Cancel", style:  UIAlertAction.Style.cancel) { (UIAlertAction) in
+        self.editAlert?.dismiss(animated: true, completion: nil)
+      }
+
+      self.editAlert?.addAction(editAction)
+      self.editAlert?.addAction(cancelAction)
+      self.present(self.editAlert!, animated: true, completion: nil)
+      
+      completionHandler(true)
+    }
+    editAction.backgroundColor = UIColor.blue
+    
+    let deleteAction = UIContextualAction(style: UIContextualAction.Style.normal, title: "Delete") { (action, view, completionHandler) in
+      self.actionType = "DELETE"
+      self.deleteAlert = UIAlertController(title: "Delete", message: self.itemAry[indexPath.row].title, preferredStyle: UIAlertController.Style.alert)
+      
+      let deleteAction = UIAlertAction(title: "Delete", style: UIAlertAction.Style.default) { (UIAlertAction) in
+        self.context.delete(self.itemAry[indexPath.row])
+        self.itemAry.remove(at: indexPath.row)
+        
+        do {
+          try self.context.save()
+        } catch {
+          print("Error saving context \(error)")
+        }
+        
+        tableView.reloadData()
+      }
+      
+      let cancelAction = UIAlertAction(title: "Cancel", style:  UIAlertAction.Style.cancel) { (UIAlertAction) in
+        self.deleteAlert?.dismiss(animated: true, completion: nil)
+      }
+      
+      self.deleteAlert?.addAction(deleteAction)
+      self.deleteAlert?.addAction(cancelAction)
+      self.present(self.deleteAlert!, animated: true, completion: nil)
+      
+      completionHandler(true)
+    }
+    deleteAction.backgroundColor = UIColor.red
+    
+    return UISwipeActionsConfiguration(actions: [deleteAction, editAction])
   }
 }
