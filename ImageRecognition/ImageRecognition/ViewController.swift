01670e1c6196721c1a5bfe53a5ad9ec1eada4cd0
chenjiahau <ivan.chen@dlinkcorp.com>
2020-12-05 09:34:51 +0800

    Init

---
+++ ImageRecognition/ImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageRecognition
+//
+//  Created by chen Ivan on 2020/12/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
